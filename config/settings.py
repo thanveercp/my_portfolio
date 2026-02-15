1d7b060634fc194e148dfba7d2e6d14179dda436
unknown <thanveercp@gmail.com>
2026-02-15 12:15:35 +0300

    Fix static files storage setting and revert python version

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles_build"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
