edfa9a33bfe674ca6854c1a78138da8f14d73867
unknown <thanveercp@gmail.com>
2026-02-15 12:14:24 +0300

    Fix build_files.sh: remove migrate, use python3.9

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,2 @@
-# build_files.sh
-pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
